3c1e7945ef3211d65b41b7a5c2a58cf02dbac365
harryleifs <148304293+harryleifs@users.noreply.github.com>
2026-09-21 14:31:09 -0500

    Update decod_7seg.vhd

--- decod_7seg.vhd
+++ decod_7seg.vhd
@@ -14,20 +14,20 @@ BEGIN
     PROCESS(bin_in, err)
     BEGIN
         IF err = '1' THEN
-            seg <= "0000110"; -- Muestra 'E'
+            seg <= "0000110"; 
         ELSE
             CASE bin_in IS
-                WHEN "0000" => seg <= "1000000"; -- 0
-                WHEN "0001" => seg <= "1111001"; -- 1
-                WHEN "0010" => seg <= "0100100"; -- 2
-                WHEN "0011" => seg <= "0110000"; -- 3
-                WHEN "0100" => seg <= "0011001"; -- 4
-                WHEN "0101" => seg <= "0010010"; -- 5
-                WHEN "0110" => seg <= "0000010"; -- 6
-                WHEN "0111" => seg <= "1111000"; -- 7
-                WHEN "1000" => seg <= "0000000"; -- 8
-                WHEN "1001" => seg <= "0010000"; -- 9
-                WHEN OTHERS => seg <= "1111111"; -- Apagado
+                WHEN "0000" => seg <= "1000000"; 
+                WHEN "0001" => seg <= "1111001"; 
+                WHEN "0010" => seg <= "0100100"; 
+                WHEN "0011" => seg <= "0110000"; 
+                WHEN "0100" => seg <= "0011001"; 
+                WHEN "0101" => seg <= "0010010"; 
+                WHEN "0110" => seg <= "0000010"; 
+                WHEN "0111" => seg <= "1111000"; 
+                WHEN "1000" => seg <= "0000000"; 
+                WHEN "1001" => seg <= "0010000"; 
+                WHEN OTHERS => seg <= "1111111";
             END CASE;
         END IF;
     END PROCESS;
