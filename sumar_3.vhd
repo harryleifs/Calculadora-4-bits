8a8b3b76d6ccae6b307b791147673c4d6250cb12
harryleifs <148304293+harryleifs@users.noreply.github.com>
2026-09-21 14:34:02 -0500

    Update sumar_3.vhd

--- sumar_3.vhd
+++ sumar_3.vhd
@@ -10,16 +10,15 @@ end sumar_3;
 
 ARCHITECTURE combinacional OF sumar_3 IS
 BEGIN
-    -- Tabla de verdad implementada con CASE para ser 100% combinacional
     PROCESS(ent)
     BEGIN
         CASE ent IS
-            WHEN "0101" => sal <= "1000"; -- 5 -> 8
-            WHEN "0110" => sal <= "1001"; -- 6 -> 9
-            WHEN "0111" => sal <= "1010"; -- 7 -> 10
-            WHEN "1000" => sal <= "1011"; -- 8 -> 11
-            WHEN "1001" => sal <= "1100"; -- 9 -> 12
-            WHEN OTHERS => sal <= ent;    -- Si es menor a 5, pasa igual
+            WHEN "0101" => sal <= "1000"; 
+            WHEN "0110" => sal <= "1001"; 
+            WHEN "0111" => sal <= "1010"; 
+            WHEN "1000" => sal <= "1011"; 
+            WHEN "1001" => sal <= "1100"; 
+            WHEN OTHERS => sal <= ent;    
         END CASE;
     END PROCESS;
 END combinacional;
