ad7b17209f6d973a7a52d23acbeaadfaa240a3a4
harryleifs <148304293+harryleifs@users.noreply.github.com>
2026-09-22 06:19:26 -0500

    Update calculadora.vhd

--- calculadora.vhd
+++ calculadora.vhd
@@ -1,12 +1,10 @@
 LIBRARY IEEE;
 USE ieee.std_logic_1164.all;
-
+USE ieee.std_logic_unsigned.all; 
 ENTITY calculadora IS
     PORT (
-        
         SW      : IN  STD_LOGIC_VECTOR(9 DOWNTO 0); 
         KEY     : IN  STD_LOGIC_VECTOR(2 DOWNTO 0); 
-		
         HEX0    : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); 
         HEX1    : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); 
         HEX2    : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); 
@@ -58,13 +56,14 @@ ARCHITECTURE estructural OF calculadora IS
     END COMPONENT;
 
     COMPONENT decod_7seg
-        port (
+        PORT (
             bin_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
             err    : IN STD_LOGIC;
             seg    : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
         );
     END COMPONENT;
 
+    
     SIGNAL op_a, op_b       : STD_LOGIC_VECTOR(3 DOWNTO 0);
     SIGNAL sel_op           : STD_LOGIC_VECTOR(1 DOWNTO 0);
     SIGNAL res_suma_resta   : STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -78,28 +77,38 @@ ARCHITECTURE estructural OF calculadora IS
     SIGNAL modo_prioridad   : STD_LOGIC_VECTOR(1 DOWNTO 0);
     SIGNAL bandera_error    : STD_LOGIC;
     SIGNAL acarreo_out      : STD_LOGIC;
-	SIGNAL modo_hex_int : STD_LOGIC;
+    SIGNAL modo_hex_int     : STD_LOGIC;
+
     
+    SIGNAL resta_a, resta_b : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL es_negativo      : STD_LOGIC;
+    SIGNAL hex1_decod       : STD_LOGIC_VECTOR(6 DOWNTO 0);
+
 BEGIN
 
     
     op_a   <= SW(3 DOWNTO 0);
     op_b   <= SW(7 DOWNTO 4);
     sel_op <= SW(9 DOWNTO 8);
+
+    
     modo_prioridad <= "10" WHEN KEY(2) = '0' ELSE  
                       "01" WHEN KEY(1) = '0' ELSE  
                       "00" WHEN KEY(0) = '0' ELSE  
                       "11";                        
+                      
     modo_hex_int <= '1' WHEN modo_prioridad = "00" ELSE '0';
-						  
+
+    
     inst_rango: detector_rango
         PORT MAP (
-        a         => op_a,
-        b         => op_b,
-        modo_hex  => modo_hex_int,
-        err_rango => bandera_error
-    );
-		
+            a         => op_a,
+            b         => op_b,
+            modo_hex  => modo_hex_int,
+            err_rango => bandera_error
+        );
+
+    
     inst_suma: sumador_restador_4b
         PORT MAP (
             a    => op_a,
@@ -110,16 +119,24 @@ BEGIN
         );
     res_suma_ext <= "000" & acarreo_out & res_suma_resta;
 
+    
+    es_negativo <= '1' WHEN (sel_op = "01" AND op_a < op_b) ELSE '0';
+
+    
+    resta_a <= op_b WHEN (op_a < op_b) ELSE op_a;
+    resta_b <= op_a WHEN (op_a < op_b) ELSE op_b;
+
     inst_resta: sumador_restador_4b
         PORT MAP (
-            a    => op_a,
-            b    => op_b,
+            a    => resta_a,
+            b    => resta_b,
             op   => '1', 
             suma => res_resta_ext(3 DOWNTO 0),
             cout => OPEN
         );
     res_resta_ext(7 DOWNTO 4) <= "0000";
 
+    
     inst_mult: multiplicador_4x4
         PORT MAP (
             a    => op_a,
@@ -127,8 +144,8 @@ BEGIN
             prod => res_mult
         );
         
-    
     res_div_ext <= (OTHERS => '0'); 
+
     inst_mux: mux_4x1
         PORT MAP (
             res_suma  => res_suma_ext,
@@ -138,20 +155,29 @@ BEGIN
             sel       => sel_op,
             salida    => resultado_bin
         );
-		
+
+    
     LEDG(7 DOWNTO 0) <= resultado_bin;
-    LEDG(8)          <= '0';
+    LEDG(8)          <= es_negativo; 
     LEDG(9)          <= bandera_error;
 
+    
     inst_bcd: conversor_bin_bcd
         PORT MAP (
             bin_in   => resultado_bin,
             bcd_unid => bcd_unidades,
             bcd_dec  => bcd_decenas
         );
+
+    
     decod_operando_a: decod_7seg PORT MAP (bin_in => op_a, err => bandera_error, seg => HEX3);
     decod_operando_b: decod_7seg PORT MAP (bin_in => op_b, err => bandera_error, seg => HEX2);
-    decod_res_dec:    decod_7seg PORT MAP (bin_in => bcd_decenas, err => bandera_error, seg => HEX1);
     decod_res_unid:   decod_7seg PORT MAP (bin_in => bcd_unidades, err => bandera_error, seg => HEX0);
+
+    
+    decod_res_dec:    decod_7seg PORT MAP (bin_in => bcd_decenas, err => bandera_error, seg => hex1_decod);
+
+    
+    HEX1 <= "0111111" WHEN es_negativo = '1' ELSE hex1_decod;
 
 END estructural;
